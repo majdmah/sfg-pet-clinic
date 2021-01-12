bc821c47f0192fc98d45a1c2ab559e11bb2af62c
Majd <majd.mahajenah@checkmarx.com>
2021-01-12 18:34:08 +0200

    refactoring to common crud interface

--- pet-clinic-data/src/main/java/com/majd/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/majd/sfgpetclinic/services/PetService.java
@@ -1,13 +1,6 @@
 package com.majd.sfgpetclinic.services;
 import com.majd.sfgpetclinic.model.Pet;
-import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet,Long>{
 
 }
